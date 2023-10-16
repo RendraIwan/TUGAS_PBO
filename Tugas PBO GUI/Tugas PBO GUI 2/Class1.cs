d6c254f5effa18188d9724a324e88ce83a3ef1e5
Rendra iwan <145772201+RendraIwan@users.noreply.github.com>
2023-10-16 20:35:44 +0700

    Add files via upload

---
+++ Tugas PBO GUI/Tugas PBO GUI 2/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_PBO_GUI_2
+{
+    public class objek
+    {
+        public string Nomer_Id {get; set;}
+        public string Barang {get; set;}
+        public string Harga {get; set;}
+        public string Pemilik {get; set;}
+        public string Kategori {get; set;}
+    }
+}
