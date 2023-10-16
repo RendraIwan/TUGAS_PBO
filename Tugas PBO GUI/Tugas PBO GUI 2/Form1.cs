132dcf1ddfc041a39a9d41f7c740ce7ef5ebaa84
Rendra iwan <145772201+RendraIwan@users.noreply.github.com>
2023-10-16 20:43:57 +0700

    Add files via upload

---
+++ Tugas PBO GUI/Tugas PBO GUI 2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_PBO_GUI_2
+{
+    public partial class Form1 : Form
+    {
+        List<objek> listobjek = new List<objek>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            objek row1 = new objek();
+            row1.Nomer_Id   = "22241011";
+            row1.Barang     = "Cangkul";
+            row1.Harga      = "150000";
+            row1.Pemilik    = "Bejo";
+            row1.Kategori   = "Pertanian";
+
+
+            objek row2 = new objek();
+            row2.Nomer_Id = "22241010";
+            row2.Barang = "Ransum";
+            row2.Harga = "300000";
+            row2.Pemilik = "Haikal";
+            row2.Kategori   = "Peternakan";
+
+            listobjek.Add(row1);
+            listobjek.Add(row2);
+            dataGridView1.DataSource = listobjek;
+        }
+
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Button_Tambah_Click(object sender, EventArgs e)
+        {
+            using (Form2 formDataBaru = new Form2())
+                if (formDataBaru.ShowDialog() == DialogResult.OK)
+                {
+                    objek newobjek = formDataBaru.GetObjek();
+                    listobjek.Add(newobjek);
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = listobjek;
+
+                    formDataBaru.Close();
+                }
+        }
+    }
+}
---
+++ Tugas PBO GUI/Tugas PBO GUI 2/Form2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_PBO_GUI_2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_Agrikultur_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Simpan_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        public objek GetObjek()
+        {
+            objek newobjek = new objek();
+            newobjek.Pemilik    = tbpemilik.Text;
+            newobjek.Nomer_Id   = tbNomerID.Text;
+            newobjek.Harga      = tbharga.Text;
+            newobjek.Barang     = tbbarang.Text;
+            newobjek.Kategori   = tbkategori.Text;
+
+            return newobjek;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void tbNomerID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbkategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Rendra Iwan Tri Meidiansyah (222410101021)");
+            Console.WriteLine("Satria Belva Nararya (222410101052)");
+            Console.WriteLine("Arief Rachman Hakim  (222410101076)");
+            Console.WriteLine("Divo Tahta Imannulloh (222410101083)");
+            Console.WriteLine("Mohammad Haikal Al Kamily (222410101092)");
+        }
+    }
+}
